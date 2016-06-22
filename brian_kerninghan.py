bc47f68e5796f6d92c2e1ab2eea841d9348a20a2
AjithPanneerselvam <ajith.ark1@gmail.com>
2016-06-22 19:21:26 +0530

    brain-k

---
+++ brian_kerninghan.py
@@ -0,0 +1,15 @@
+"""
+Brian Kerninghan - Count the number of set bits in an integer
+Time Complexity - O(log n)
+Key idea - n & (n-1)
+"""
+
+def brian_kerninghan(n):
+    count = 0
+    while (n != 0):
+        n = n & (n-1)
+        count += 1
+    return count
+
+n = int(raw_input("Enter any interger \n"))
+print brian_kerninghan(n)
