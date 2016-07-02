d6404e94604c3935ec959e2bbb7f390f4f2b7f77
AjithPanneerselvam <ajith.ark1@gmail.com>
2016-07-03 01:08:22 +0530

    subset dynamic programming

--- sum_subset.py
+++ sum_subset.py
@@ -1,4 +1,32 @@
 """
-
-
+Sum of subset problems using dynamic programming (To find whether the target is present as the sum of the subset given)
+Time complexity - O(sum * size_of_subset)
+Space complexity - O(sum * size_of_subset)
 """
+
+def sum_subset(target,subset):
+	n = len(subset)
+	matrix = [ (target + 1) * [False] for i in range(n+1)]
+	j = 0
+
+	for i in range(n+1):
+		matrix[i][j] = True
+
+	for i in range(1,n+1):
+		for j in range(1,target+1):
+			if subset[i-1] > j:
+				matrix[i][j] = matrix[i-1][j]
+			elif subset[i-1] == j:
+				matrix[i][j] = True
+			else:
+				matrix [i][j] = matrix[i-1][j-subset[i-1]]
+	
+	return matrix[n][target]
+
+
+if __name__ == '__main__':
+	target = input()
+	subset = []
+	subset = map(int,raw_input().strip().split())
+	print sum_subset(target,subset)
+
