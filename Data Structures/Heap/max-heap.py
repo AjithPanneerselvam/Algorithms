611ed4b406ba0ce7c430372379abab99fdcbc267
AjithPanneerselvam <ajithpanneerselvam@live.com>
2017-08-18 23:36:34 +0530

    update

--- Data Structures/Heap/max-heap.py
+++ Data Structures/Heap/max-heap.py
@@ -3,10 +3,22 @@ Maximum Heap
 """
 
 class BinaryHeap:
-    def __init__(self, size = 0):
-        self.size = size
+    def __init__(self):
+        self.size = 0
         # '#' represents that index 0 is invalid
         self.heap = ['#']
+
+
+    def heapify(self):
+        """
+        This method is used to preserve the Heap property by swapping elements.
+        """
+        j = len(self.heap) - 1
+        parent = int(j / 2)
+        while(parent >= 1 and self.heap[parent] < self.heap[j]):
+            self.heap[parent], self.heap[j] = self.heap[j], self.heap[parent]
+            j = parent
+            parent = int(parent / 2)
 
 
     def insert(self, data):
@@ -15,48 +27,70 @@ class BinaryHeap:
         self.heapify()
 
 
-    def heapify(self):
-        j = len(self.heap) - 1
-        parent = int(j / 2)
+    def maxChildIndex(self, index):
+        if ((index * 2) + 1) <= self.size:
+            if self.heap[(index * 2) + 1] > self.heap[index * 2]:
+                return ((index * 2) + 1)
+            elif self.heap[(index * 2) + 1] < self.heap[index * 2]:
+                return (index * 2)
+            else:
+                # return any node, if both the children has same value
+                return (index * 2)
+        elif (index * 2) <= self.size:
+            return (index * 2)
+        else:
+            # No children for the given Node
+            return (-1)
 
-        while(parent >= 1 and self.heap[parent] < self.heap[j] ):
-            self.heap[parent], self.heap[j] = self.heap[j], self.heap[parent]
-            j = parent
-            parent = int(parent / 2)
+
+    def percolateDown(self, index):
+        while(index * 2 <= self.size):
+            maxChildIndex = self.maxChildIndex(index)
+            if self.heap[index] < self.heap[maxChildIndex]:
+                self.heap[index], self.heap[maxChildIndex] = self.heap[maxChildIndex], self.heap[index]
+            index = maxChildIndex
 
 
-    def extractMax():
-        pass
+    def extractMax(self):
+        if self.size == 0:
+            return
+
+        self.size = self.size - 1
+        if self.size == 1:
+            return self.heap.pop()
+        maxElement = self.heap[1]
+        self.heap[1] = self.heap[-1]
+        self.heap.pop()
+        self.percolateDown(1)
+        return maxElement
+
+
+    def peekMax(self):
+        if self.size == 0:
+            return
+        return self.heap[1]
 
 
     def printHeap(self):
         if self.size == 0:
             return
-        parent = 1
-        lastParent = (self.size / 2)
-
-        while(parent < lastParent):
-                print ("Parent ", self.heap[parent], end = ' ')
-                leftChild = 2 * parent
-
-                if leftChild < self.size:
-                    print("Left Child ", self.heap[leftChild], end = ' ')
-                    rightChild = leftChild + 1
-                    if rightChild < self.size:
-                        print("Right Child ", self.heap[rightChild], end = ' ')
-
-                parent = parent * 2
+        print (self.heap)
 
 
-                                ### Testcases ###
+#                                 ### Testcases ###
 # heapObj = BinaryHeap()
 # heapObj.insert(9)
-# heapObj.insert(8)
-# heapObj.insert(7)
-# heapObj.insert(6)
 # heapObj.insert(5)
 # heapObj.insert(4)
 # heapObj.insert(3)
+# heapObj.insert(8)
+# heapObj.insert(7)
+# heapObj.insert(6)
 # heapObj.printHeap()
 # heapObj.insert(11)
 # heapObj.printHeap()
+# heapObj.insert(10)
+# heapObj.printHeap()
+# print (heapObj.extractMax())
+# heapObj.printHeap()
+# print (heapObj.peekMax())
