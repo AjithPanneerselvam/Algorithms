1037d419a91ac0ce751cb9915e022b94738bc24e
AjithPanneerselvam <ajithpanneerselvam@live.com>
2017-08-22 01:01:35 +0530

    add testcases

--- Data Structures/Graph Theory/graph.py
+++ Data Structures/Graph Theory/graph.py
@@ -3,9 +3,8 @@ Implementation of Graph Data Structure
 """
 
 class Graph(object):
-
-    def __init__(self,graph = None):
-        self.graph = graph
+    def __init__(self):
+        self.graph = {}
 
 
     def vertices(self):
@@ -16,7 +15,6 @@ class Graph(object):
     def edges(self):
         # Return edges in the graph
         edges = []
-
         for vert in self.graph:
             for verti in self.graph[vert]:
                 if {vert,verti} not in edges:
@@ -25,11 +23,10 @@ class Graph(object):
 
 
     # Add edge in an undirected graph
-    def undirAddEdge(self,edge):
+    def undirAddEdge(self, edge):
         # An edge is added
         vertices = set(edge)
         vertex1,vertex2 = tuple(vertices)
-
         if vertex1 not in self.graph:
             self.graph[vertex1] = [vertex2]
         else:
@@ -41,8 +38,22 @@ class Graph(object):
             self.graph[vertex2].append(vertex1)
 
 
+    # Add edge in an undirected graph along with edge weight
+    def undirAddEdgeWeight(self, edge, weight):
+        vertex1, vertex2 = edge[0], edge[1]
+        if vertex1 not in self.graph:
+            self.graph[vertex1] = [vertex2, weight]
+        else:
+            self.graph[vertex1].append([vertex2, weight])
+
+        if vertex2 not in self.graph:
+            self.graph[vertex2] = [vertex1, weight]
+        else:
+            self.graph[vertex2].append(vertex1, weight)
+
+
     # Add edge in a directed graph
-    def dirAddEdge(self,edge):
+    def dirAddEdge(self, edge):
         # vertex1 -> vertex2
         vertex1, vertex2 = edge[0], edge[1]
         if vertex1 in self.graph:
@@ -54,44 +65,14 @@ class Graph(object):
             self.graph[vertex2] = []
 
 
-def undirSampleTestcases():
+    # Add edge in a directed graph along with edge weight
+    def dirAddEdgeWeight(self, edge, weight):
+        # vertex1 -> vertex2
+        vertex1, vertex2 = edge[0], edge[1]
+        if vertex1 in self.graph:
+            self.graph[vertex1].append([vertex2, weight])
+        else:
+            self.graph[vertex1] = [vertex2, weight]
 
-    graph_obj = Graph({})
-
-    graph_obj.undirAddEdge(['a','b'])
-    graph_obj.undirAddEdge(['a','d'])
-    graph_obj.undirAddEdge(['b','c'])
-    graph_obj.undirAddEdge(['b','f'])
-    graph_obj.undirAddEdge(['c','f'])
-    graph_obj.undirAddEdge(['c','g'])
-    graph_obj.undirAddEdge(['d','e'])
-    graph_obj.undirAddEdge(['e','g'])
-
-    return graph_obj
-
-
-def dirSampleTestcases():
-
-    graph_obj = Graph({})
-
-    graph_obj.dirAddEdge(['a','b'])
-    graph_obj.dirAddEdge(['b','c'])
-    graph_obj.dirAddEdge(['c','g'])
-    graph_obj.dirAddEdge(['b','f'])
-    graph_obj.dirAddEdge(['c','f'])
-    graph_obj.dirAddEdge(['a','d'])
-    graph_obj.dirAddEdge(['d','e'])
-    graph_obj.dirAddEdge(['e','g'])
-
-    return graph_obj
-
-
-#                                         ### Sample testcases ###
-#
-# undir_graph_obj = undirSampleTestcases()
-# print undir_graph_obj.vertices(), undir_graph_obj.edges()
-# print "Undirected Graph" , undir_graph_obj.graph
-#
-# dir_graph_obj = dirSampleTestcases()
-# print dir_graph_obj.vertices(), dir_graph_obj.edges()
-# print "Directed Graph" , dir_graph_obj.graph
+        if vertex2 not in self.graph:
+            self.graph[vertex2] = []
